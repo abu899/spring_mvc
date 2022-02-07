bb09f0ad6139a760ab31033f07aadd2bd7ca0b14
Ahn <abu89@naver.com>
2022-02-07 17:12:02 +0900

    [Backend Basic] MVC with servlet and JSp
     + make MVC pattern
       + servlet(Controller, Model)
       + JSP(View)

---
+++ backend_basic/src/main/java/hello/mvc/servletmvc/MvcMemberListServlet.java
@@ -0,0 +1,31 @@
+package hello.mvc.servletmvc;
+
+import hello.mvc.servlet.domain.member.Member;
+import hello.mvc.servlet.domain.member.MemberRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/member/save")
+public class MvcMemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Member> members = memberRepository.findAll();
+
+        request.setAttribute("members", members);
+
+        String writePath = "/WEB-INF/views/";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(writePath);
+        dispatcher.forward(request, response);
+    }
+}
